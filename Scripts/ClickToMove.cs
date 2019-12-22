06cc47d69e8cca376faa5ac0bd3e6d3da05f54aa
M-T-Asagi <t.asagi00+github@gmail.com>
2019-12-22 22:28:47 +0900

    add scripts

---
+++ Scripts/ClickToMove.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class ClickToMove : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent navMeshAgent = null;
+
+    [SerializeField]
+    float approacheLimit = 0.3f;
+
+    [SerializeField]
+    float reamingDistanceRange = 0.3f;
+
+    public class WalkingStopEventArgs : System.EventArgs
+    {
+        public Vector3 nowPos;
+        public Vector3 targetPos;
+
+        public WalkingStopEventArgs(Vector3 _nowPos, Vector3 _targetPos)
+        {
+            nowPos = _nowPos;
+            targetPos = _targetPos;
+        }
+    }
+
+    public System.EventHandler<WalkingStopEventArgs> WalkingStop;
+
+    public bool Walking { get { return walking; }}
+    bool walking = false;
+
+    Vector3 nowTarget;
+    public Vector3 NowTarget { get { return nowTarget; }}
+
+    public void SetNavigateTo(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData)data;
+        SetNavigateTo(pointerData.pointerPressRaycast.worldPosition);
+    }
+
+    public void SetNavigateTo(Vector3 point)
+    {
+        nowTarget = point;
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(point);
+        StartCoroutine(WalkingTruing());
+    }
+
+    IEnumerator WalkingTruing()
+    {
+        yield return new WaitForEndOfFrame();
+        walking = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(walking && (Mathf.Abs(navMeshAgent.remainingDistance) <= reamingDistanceRange || Vector3.Distance(nowTarget, navMeshAgent.transform.position) <= approacheLimit))
+        {
+            Stop();
+        }
+    }
+
+    public void Stop()
+    {
+        Debug.Log("Stopped");
+
+        navMeshAgent.isStopped = true;
+        walking = false;
+        WalkingStop?.Invoke(this, new WalkingStopEventArgs(transform.position, nowTarget));
+    }
+
+    public void SetNavMeshAgent(NavMeshAgent _navMeshAgent)
+    {
+        navMeshAgent = _navMeshAgent;
+    }
+}
---
+++ Scripts/TimeScaledEffectMonoBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsagiHandyScripts
+{
+    public class TimeScaledProcessionMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        [Range(0.0f, 10.0f)]
+        protected float timeScale = 1.0f;
+
+        [SerializeField]
+        [Range(0.0f, 10.0f)]
+        protected float Resolution = 1.0f;
+
+        private float lastProcessedTime = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            lastProcessedTime = CalculatedTimeWithResolution();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            float nowTime = CalculatedTimeWithResolution();
+            if (nowTime != lastProcessedTime)
+            {
+                UpdateProcess(nowTime);
+                lastProcessedTime = nowTime;
+            }
+        }
+
+        private float CalculatedTimeWithResolution()
+        {
+            float nowTime = Time.time;
+            if (Time.timeScale == 0)
+                nowTime = lastProcessedTime;
+            else
+                nowTime *= timeScale;
+
+            if (Resolution != 0)
+                nowTime = Mathf.Floor(nowTime / Resolution) * Resolution;
+
+            return nowTime;
+        }
+
+        virtual protected void UpdateProcess(float time) { }
+    }
+}
